9c54d2419c18cd37f1efc37efbc02d6536967584
Timothy Barnes <barnes7td@gmail.com>
2012-10-04 23:36:01 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RudysWebsite::Application.config.secret_token = 'ae1c35c7cef8fa0961228111bff001e00c9d4d81c61d5702782fe8e94713ec7ebd5b0efba5c24de3cdb9c4b164989869914b955d087d6692c9409c96133d6484'
