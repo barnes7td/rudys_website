7d44560192fd5a886860fe623536cf3245ade7a2
Timothy Barnes <barnes7td@gmail.com>
2012-10-12 00:36:54 -0400

    Made Footer pattern teaser dynamic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_patterns
+
+  def set_patterns
+    @patterns = Pattern.all limit: 6
+  end
+
 end
