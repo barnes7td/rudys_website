bf59e163c38ad3e7f58c96d1c37696702004a206
Timothy Barnes <barnes7td@gmail.com>
2012-10-12 01:02:04 -0400

    Change url to show name of pattern

--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -2,4 +2,9 @@ class Pattern < ActiveRecord::Base
   attr_accessible :description, :image, :price, :title
   has_many :pattern_posts
   has_many :orders
+
+  #For no id, use friendly_id gem. Watch Railscast #314.
+  def to_param
+    "#{id} #{title}".parameterize
+  end
 end
