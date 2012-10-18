d60e0425d443c1489e4a273a1a540f7473df1431
Timothy Barnes <barnes7td@gmail.com>
2012-10-18 09:55:31 -0400

    Fix repo. Add Pattern new route

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -1,9 +1,21 @@
 class PatternsController < ApplicationController
+
   def index
     @patterns = Pattern.all
   end
 
   def show
-    @pattern = Pattern.find_by_title(params[:title])
+    @pattern = Pattern.find_by_title(params[:id])
   end
+
+  def new
+    @pattern = Pattern.new
+  end
+
+  def create
+    @pattern = Pattern.new(params[:pattern])
+    @pattern.save
+    redirect_to patterns_path
+  end
+
 end
