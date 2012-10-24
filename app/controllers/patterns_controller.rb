56f09d9fe6ceaee16fdb43212e06dceb62e5759b
Timothy Barnes <barnes7td@gmail.com>
2012-10-23 22:18:24 -0400

    Add Edit and Delete on Patterns#index

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -18,4 +18,23 @@ class PatternsController < ApplicationController
     redirect_to patterns_path
   end
 
+  def edit
+    @pattern = Pattern.find(params[:id])
+  end
+
+  def update
+    pattern = Pattern.find(params[:id])
+    if pattern.update_attributes(params[:pattern])
+      flash[:notice] = "User successfully updated"
+      redirect_to patterns_path
+    else
+      render :action => "show", :title => :pattern
+    end
+  end
+
+  def destroy
+    Pattern.find(params[:id]).destroy
+    redirect_to patterns_path
+  end
+
 end
