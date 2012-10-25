39c8bf2e31d0c58f5da5ad98766c493773b33ffc
Timothy Barnes <barnes7td@gmail.com>
2012-10-25 00:51:41 -0400

    Fixed CSS for Nav and Footer

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ RudysWebsite::Application.routes.draw do
 
   #resources :users       #, :except => [:destroy, :create, :new, :edit, :update]
 
+  resources :users
   resources :patterns
 
   match "patterns/:title" => "pattern#show"
