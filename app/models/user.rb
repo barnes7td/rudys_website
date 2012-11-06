d26c0374797098842498f00744e27ab00b181590
Timothy <barnes7td@gmail.com>
2012-11-05 21:01:00 -0500

    Fix Name presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,6 @@ class User < ActiveRecord::Base
 
   validates :name, :presence => true
   validates :email, :presence => true
-  validates :password, :presence => true
-  validates :password_confirmation, :presence => true
+  # validates :password, :presence => true
+  # validates :password_confirmation, :presence => true
 end
